b2eab96fdb8f4bc22df4b76f2e258665501f8545
Dparamet <dparamet1@gmail.com>
2025-09-28 14:09:33 +0700

    test 5

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 3000;
 // ให้ backend serve frontend ด้วย
 app.use(express.static(path.join(__dirname, "frontend")));
 
+app.get("/api/threads", (req, res) => {
+  const threads = [
+    { id: 1, title: "Hello Forum", body: "This is the first thread!" },
+    { id: 2, title: "Question 1", body: "Here's a thread about a question." },
+  ];
+  res.json(threads);
+});
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
